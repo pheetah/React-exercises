620fa38990abcefebb5eadb59082ec346dfaf04b
Eyüp Fatih Ersoy <eyupfatih.ersoy@hotmail.com>
2021-07-26 17:14:34 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useState,useEffect } from 'react';
 
 function App() {
+
+  const [imdbFilms, setImbdFilms] = useState([]);
+
+  useEffect(() =>{
+
+    let params = {q: 'game of thr'};
+
+    axios.get('https://imdb8.p.rapidapi.com/auto-complete',
+      {
+        params: params,
+        headers: {
+          'x-rapidapi-key': '90c2e39d65msh0b7d914f1a20a7dp1d42aejsn5689990ba931',
+          'x-rapidapi-host': 'imdb8.p.rapidapi.com'
+        }
+      }
+    )
+    .then(filmsResponse => {
+        setImbdFilms(filmsResponse.data.d);
+        console.log(filmsResponse);
+    });
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      {imdbFilms.map(film => <li>{film.l}</li>)}
+    </ul>
   );
 }
 
