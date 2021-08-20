ffb30d2528084f22730ec6f31b5f48bd909bce5b
Eyüp Fatih Ersoy <eyupfatih.ersoy@hotmail.com>
2021-08-20 11:35:39 +0300

    release v1

--- src/components/cart/ShoppingCart.js
+++ src/components/cart/ShoppingCart.js
@@ -12,9 +12,13 @@ const ShopCart = props => {
     const totalAmount = `$${cartContext.totalAmount.toFixed(2)}`;
     const hasItems = cartContext.items.length > 0;
 
-    const cartItemRemoveHandler = (id) => {};
+    const cartItemRemoveHandler = (id) => {
+        cartContext.removeItem(id);
+    };
 
-    const cartItemAddHandler = (item) => {};
+    const cartItemAddHandler = (item) => {
+        cartContext.addItem({...item, amount:1});
+    };
 
     const cartItems = (
         <ul className={classes["cart-items"]}>
--- src/components/layout/HeaderCart.js
+++ src/components/layout/HeaderCart.js
@@ -1,17 +1,38 @@
 import CartIcon from '../cart/Cart';
 import classes from './HeaderCart.module.css';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CartContext from '../../store/cart-context';
 
 const HeaderCart = props => {
 
-    const cartContext = useContext(CartContext); 
+    const[btnIsHighlighted, setBtnIsHighLighted] = useState(false);
 
-    const numberOfCartItems = cartContext.items.reduce((curNumber, item) => {
+    const cartContext = useContext(CartContext); 
+    const { items } = cartContext;
+
+    const numberOfCartItems = items.reduce((curNumber, item) => {
         return curNumber + item.amount
     }, 0);
 
-    return <button className={classes.button} onClick={props.onClick}>
+
+    const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+
+    useEffect(() => {
+        if(items.length === 0){
+            return;
+        }
+        setBtnIsHighLighted(true);
+
+        const timer = setTimeout(() => {
+            setBtnIsHighLighted(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        }
+    }, [items]);
+
+    return <button className={btnClasses} onClick={props.onClick}>
         <span className={classes.icon}>
             <CartIcon />
         </span>
